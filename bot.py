9a731609524e4c8a5a60a1c93fa159c91e102453
pavan <pavanteja.knp5@gmail.com>
2025-06-18 16:45:24 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+from telegram.ext import Updater, CommandHandler
+from telegram import InputFile
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+def send_resume(update, context):
+    chat_id = update.message.chat_id
+    with open("resume.pdf", "rb") as file:
+        context.bot.send_document(chat_id=chat_id, document=InputFile(file), filename="Pavan_Resume.pdf")
+
+updater = Updater(BOT_TOKEN, use_context=True)
+dp = updater.dispatcher
+dp.add_handler(CommandHandler("resume", send_resume))
+updater.start_polling()
+updater.idle()
